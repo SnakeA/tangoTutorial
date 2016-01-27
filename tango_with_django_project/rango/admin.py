2afd5e8e88d1dd0ad9c5b42c267647cce33517d7
SnakeA <1200048c@student.gla.ac.uk>
2016-01-27 22:01:36 +0000

    Completed Chapter 6 + exercise

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from rango.models import Category, Page
+
+
+# This is to override the default "Page" display in the Admin interface
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
 
 # Register your models here.
+admin.site.register(Category)
+# In order to register the updated interface
+admin.site.register(Page, PageAdmin)
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+#Django creates an ID field for you automatically in each table relating to a model.
+#You therefore do not need to explicitly define a primary key for each model  its done for you
+
+class Category (models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    #Like Java's toString() - provides a unicode representation of the model
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural="Categories"
+
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField(null=True)
+    views = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
